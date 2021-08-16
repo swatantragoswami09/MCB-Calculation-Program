f6c44be5d382971000f15a09ce818781931992bc
Swatantra <54662673+swatantragoswami09@users.noreply.github.com>
2021-08-16 22:29:05 +0530

    Update MCB.cpp

--- MCB.cpp
+++ MCB.cpp
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include<bits/stdc++.h>
 using namespace std;
@@ -13,6 +11,10 @@ int MCB(int a[],int size){
     for(int i=0;i<size-1;i++){
        sum=sum+a[i];
        
+    }
+    if(sum<3000){
+        cout<<"Your MCB is less than 3000\n";
+        return 0;
     }
     
     cout<<"\nTotal MCB = "<<sum<<"\n";
@@ -38,6 +40,6 @@ int MCB(int a[],int size){
 int main()
 {
     // int a[3]={1935,1964,1592};
-    int a[3]={1935,1964,1592};
+    int a[2]={5491,1749};
     cout<<"\nYour Cheque = "<<MCB(a,3)-MCB(a,3)*0.05;
 }
